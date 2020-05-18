755720b2cdd180cafb932b787d3307c60a34b7f1
Zen <46526140+master-of-zen@users.noreply.github.com>
2020-05-18 13:17:21 +0300

    Update to current Av1an version

--- plot_vmaf.py
+++ plot_vmaf.py
@@ -1,30 +1,45 @@
 #!/usr/bin/env python3
+
 import sys
-from matplotlib import pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 filename = sys.argv[1]
 
-with open(filename,'r') as f:
-    file = f.readlines()
-    file = [x.strip() for x in file if 'vmaf="' in x]
-    vmafs = []
-    for i in file:
-        vmf = i[i.rfind('="') + 2: i.rfind('"')]
-        vmafs.append(float(vmf))
+def read_vmaf_xml(file):
+        with open(file, 'r') as f:
+            file = f.readlines()
+            file = [x.strip() for x in file if 'vmaf="' in x]
+            vmafs = []
+            for i in file:
+                vmf = i[i.rfind('="') + 2: i.rfind('"')]
+                vmafs.append(float(vmf))
 
-    vmafs = [round(float(x), 3) for x in vmafs if type(x) == float]
-    plt.ylim((int(min(vmafs)), 100))
-    d = min(vmafs)
-    for i in range(int(d), 100, 1):
-        plt.axhline(i, color='grey', linewidth=0.5)
+            vmafs = [round(float(x), 3) for x in vmafs if type(x) == float]
 
-x = [x for x in range(len(vmafs))]
-plt.plot(x, vmafs)
+        # Data
+        x = [x for x in range(len(vmafs))]
+        mean = round(sum(vmafs) / len(vmafs), 3)
+        perc_1 = round(np.percentile(vmafs, 1), 3)
+        perc_25 = round(np.percentile(vmafs, 25), 3)
+        perc_75 = round(np.percentile(vmafs, 75), 3)
 
-# Save/close
-plt.ylabel('VMAF')
-plt.xlabel('Frames')
-plt.title(f'{sys.argv[1]}, {len(vmafs)} ')
-plt.tight_layout()
-plt.savefig('fig', dpi=1000)
+        # Plot
+        plt.figure(figsize=(15, 4))
+        [plt.axhline(i, color='grey', linewidth=0.4) for i in range(0, 100)]
+        [plt.axhline(i, color='black', linewidth=0.6) for i in range(0, 100, 5)]
+        plt.plot(x, vmafs, label=f'Frames: {len(vmafs)}\nMean:{mean}'
+                                 f'\n1%: {perc_1} \n25%: {perc_25} \n75%: {perc_75}', linewidth=0.7)
+        plt.ylabel('VMAF')
+        plt.legend(loc="lower right")
+        plt.ylim(int(perc_1), 100)
+        plt.tight_layout()
+        plt.margins(0)
+
+        # Save
+        file_name = 'plot.png'
+        plt.savefig(file_name, dpi=500)
+
+read_vmaf_xml(filename)
+
