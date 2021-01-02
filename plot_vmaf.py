5e16d877a9f323e331a703a2824c07d613ffd0d5
Zen <true.grenight@gmail.com>
2021-01-02 04:13:09 +0200

    reworked to json

--- plot_vmaf.py
+++ plot_vmaf.py
@@ -3,7 +3,14 @@
 import sys, argparse
 import numpy as np
 import matplotlib.pyplot as plt
+import json
 from math import log10
+
+def read_json(file):
+    with open(file, 'r') as f:
+        fl = json.load(f)
+        return fl
+
 
 def plot_vmaf(vmafs):
     # Create datapoints
@@ -21,7 +28,7 @@ def plot_vmaf(vmafs):
     [plt.axhline(i, color='black', linewidth=0.6) for i in range(0, 100, 5)]
     plt.plot(x, vmafs, label=f'Frames: {len(vmafs)} Mean:{mean}\n'
                                 f'1%: {perc_1}  25%: {perc_25}  75%: {perc_75}', linewidth=0.7)
-    
+
     plt.plot([1, plot_size], [perc_1, perc_1], '-', color='red')
     plt.annotate(f'1%: {perc_1}', xy=(0, perc_1), color='red')
 
@@ -42,30 +49,18 @@ def plot_vmaf(vmafs):
     # Save
     plt.savefig(args.output, dpi=500)
 
-def read_vmaf_xml(file):
-    with open(file, 'r') as f:
-        file = f.readlines()
-        file = [x.strip() for x in file if 'vmaf="' in x]
-        vmafs = []
-        for i in file:
-            vmf = i[i.rfind('="') + 2: i.rfind('"')]
-            vmafs.append(float(vmf))
-
-        vmafs = [round(float(x), 3) for x in vmafs if type(x) == float]
-
-    return(vmafs)
-
 def main():
-    vmafs = read_vmaf_xml(args.vmaf_file)
+    jsn = read_json(args.vmaf_file)
+    vmafs = [x['metrics']['vmaf'] for x in jsn['frames']]
     plot_vmaf(vmafs)
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Plot vmaf to graph')
     parser.add_argument('vmaf_file', type=str, help='Vmaf log file')
     parser.add_argument('-o','--output', dest='output', type=str, default='plot.png', help='Graph output filename (default plot.png)')
-    
+
     return(parser.parse_args())
 
 if __name__ == "__main__":
     args = parse_arguments()
-    main()    
+    main()
